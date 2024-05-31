c48b986b7f8cea311e821f0c1e58096ef9a6b506
lehuygiang28 <lehuygiang28@gmail.com>
2024-05-31 20:27:25 +0700

    refactor: :lipstick: format & lint

--- apps/be/webpack.config.js
+++ apps/be/webpack.config.js
@@ -2,18 +2,18 @@ const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/be'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-    }),
-  ],
+    output: {
+        path: join(__dirname, '../../dist/apps/be'),
+    },
+    plugins: [
+        new NxAppWebpackPlugin({
+            target: 'node',
+            compiler: 'tsc',
+            main: './src/main.ts',
+            tsConfig: './tsconfig.app.json',
+            assets: ['./src/assets'],
+            optimization: false,
+            outputHashing: 'none',
+        }),
+    ],
 };
--- apps/fe-e2e/cypress.config.ts
+++ apps/fe-e2e/cypress.config.ts
@@ -3,12 +3,12 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
-      webServerCommands: { default: 'nx run fe:start' },
-      ciWebServerCommand: 'nx run fe:serve-static',
-    }),
-    baseUrl: 'http://localhost:3000',
-  },
+    e2e: {
+        ...nxE2EPreset(__filename, {
+            cypressDir: 'src',
+            webServerCommands: { default: 'nx run fe:start' },
+            ciWebServerCommand: 'nx run fe:serve-static',
+        }),
+        baseUrl: 'http://localhost:3000',
+    },
 });
