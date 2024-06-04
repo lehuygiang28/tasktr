78021201b508224d1dcc87d12dabcedac7faba1a
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-05 06:53:36 +0700

    chore(be): :recycle: re-order import

--- apps/be/src/app/tasks/tasks.module.ts
+++ apps/be/src/app/tasks/tasks.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BullModule } from '@nestjs/bullmq';
 import { HttpModule } from '@nestjs/axios';
+
+import { BULLMQ_TASK_QUEUE, TaskProcessor } from '~be/common/bullmq';
+
+import { Task, TaskSchema } from './schemas';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
-import { TaskProcessor, BULLMQ_TASK_QUEUE } from '~be/common/bullmq';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Task, TaskSchema } from './schemas/task.schema';
 
 @Module({
     imports: [
@@ -15,7 +17,7 @@ import { Task, TaskSchema } from './schemas/task.schema';
             name: BULLMQ_TASK_QUEUE,
             connection: {
                 host: process.env['REDIS_HOST'] ?? '',
-                port: Number(process.env['REDIS_PORT']) || 6379,
+                port: Number(process.env['REDIS_PORT']),
                 password: process.env['REDIS_PASSWORD'],
             },
         }),
