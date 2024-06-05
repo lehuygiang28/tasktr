d935a38d41779327afdb72b35684d019bf447281
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-05 07:10:42 +0700

    feat(be): :sparkles: register bull background job as root

--- apps/be/src/app/app.module.ts
+++ apps/be/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BullModule } from '@nestjs/bullmq';
 
 import { MongodbModule } from '~be/common/mongodb';
 import { LoggerModule } from '~be/common/pino-logger';
 import { I18nModule } from '~be/common/i18n';
 import { RedisModule } from '~be/common/redis';
-import { BULLMQ_BG_JOB_QUEUE, BackgroundProcessor } from '~be/common/bullmq';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,13 +18,16 @@ import { TasksModule } from './tasks/tasks.module';
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        BullModule.registerQueue({
-            name: BULLMQ_BG_JOB_QUEUE,
-            connection: {
-                host: process.env['REDIS_HOST'] ?? '',
-                port: Number(process.env['REDIS_PORT']),
-                password: process.env['REDIS_PASSWORD'],
-            },
+        BullModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                connection: {
+                    host: configService.get<string>('REDIS_HOST'),
+                    port: configService.get<number>('REDIS_PORT'),
+                    password: configService.get<string>('REDIS_PASSWORD'),
+                },
+            }),
+            inject: [ConfigService],
         }),
         LoggerModule,
         I18nModule,
@@ -36,7 +38,6 @@ import { TasksModule } from './tasks/tasks.module';
         TasksModule,
     ],
     controllers: [AppController],
-    providers: [AppService, BackgroundProcessor],
-    exports: [BackgroundProcessor],
+    providers: [AppService],
 })
 export class AppModule {}
