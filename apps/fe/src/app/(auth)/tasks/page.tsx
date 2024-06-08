9b235dd410cdc5480eb93d9de2089793c742aac7
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-08 16:33:39 +0700

    feat(fe): :zap: improve task list performance and add search functionality

--- apps/fe/src/app/(auth)/tasks/page.tsx
+++ apps/fe/src/app/(auth)/tasks/page.tsx
@@ -1,33 +1,68 @@
 'use client';
 
-import { useUpdate } from '@refinedev/core';
-import { DeleteButton, EditButton, List, ShowButton, useTable } from '@refinedev/antd';
-import { Space, Table, Switch, Typography } from 'antd';
+import { HttpError, useUpdate } from '@refinedev/core';
+import { DeleteButton, EditButton, List, SaveButton, ShowButton, useTable } from '@refinedev/antd';
+import { Space, Table, Switch, Typography, Form, Input } from 'antd';
 import { toString as cronReadable } from 'cronstrue';
+import { getSchedule, stringToArray } from 'cron-converter';
 
 import { type TaskDto } from '~be/app/tasks/dtos';
 import { useDebouncedCallback } from '~/hooks/useDebouncedCallback';
 import { HttpMethodTag } from '~/components/tag/http-method-tag';
+import { HttpMethodEnum } from '~be/app/tasks/tasks.enum';
+import { SearchOutlined } from '@ant-design/icons';
 
 const { Text } = Typography;
 
+interface TableSearch {
+    name?: string;
+}
+
 export default function TaskList() {
-    const { tableProps } = useTable<TaskDto>({
+    const { tableProps, searchFormProps } = useTable<TaskDto, HttpError, TableSearch>({
         syncWithLocation: true,
     });
-
     const { mutate: update } = useUpdate<TaskDto>({});
-
     const debouncedUpdate = useDebouncedCallback(update, 200);
 
     return (
         <List>
-            <Table<TaskDto> {...tableProps} rowKey="_id">
-                <Table.Column dataIndex="name" title={'Name'} />
+            <Form {...searchFormProps} layout="inline">
+                <Form.Item name="name">
+                    <Input placeholder="Search by name" />
+                </Form.Item>
+                <SaveButton onClick={searchFormProps.form?.submit} icon={<SearchOutlined />}>
+                    Search
+                </SaveButton>
+            </Form>
+            <br />
+            <Table<TaskDto>
+                {...tableProps}
+                rowKey="_id"
+                pagination={{
+                    ...tableProps.pagination,
+                    position: ['bottomCenter'],
+                    size: 'small',
+                }}
+            >
+                <Table.Column
+                    dataIndex="name"
+                    title={'Name'}
+                    onFilter={(value, record) => record.name.indexOf(value as string) === 0}
+                    sorter={(a: TaskDto, b: TaskDto) => a.name.localeCompare(b.name)}
+                    sortDirections={['descend', 'ascend']}
+                />
                 <Table.Column
                     dataIndex="method"
                     title={'Method'}
                     render={(method) => <HttpMethodTag method={method} />}
+                    filters={Object.values(HttpMethodEnum).map((value: string) => ({
+                        value: value,
+                        text: <HttpMethodTag method={value} />,
+                    }))}
+                    onFilter={(value, record) => record.method.indexOf(String(value)) === 0}
+                    sorter={(a: TaskDto, b: TaskDto) => a.method.localeCompare(b.method)}
+                    sortDirections={['descend', 'ascend']}
                 />
                 <Table.Column
                     dataIndex="cron"
@@ -35,7 +70,9 @@ export default function TaskList() {
                     render={(_, record: TaskDto) => (
                         <>
                             <Space direction="vertical">
-                                <Text>{record.cron}</Text>
+                                <Text>
+                                    <pre style={{ display: 'inline' }}>{record.cron}</pre>
+                                </Text>
                                 <Text type="secondary">
                                     {cronReadable(record.cron, {
                                         throwExceptionOnParseError: false,
@@ -46,6 +83,41 @@ export default function TaskList() {
                             </Space>
                         </>
                     )}
+                    filters={[
+                        { text: 'More than once a day', value: 'more-once-day' },
+                        { text: 'Less than once a day', value: 'less-once-day' },
+                        { text: 'Every hour', value: 'hour' },
+                        { text: 'Every day', value: 'day' },
+                        { text: 'Every week', value: 'week' },
+                    ]}
+                    onFilter={(value, record) => {
+                        const schedule = getSchedule(stringToArray(record.cron));
+                        const nextRun1 = schedule.next();
+                        const nextRun2 = schedule.next();
+                        const diffInHours = Math.abs(nextRun1 - nextRun2) / 36e5;
+
+                        switch (value) {
+                            case 'more-once-day':
+                                return diffInHours < 24;
+                            case 'less-once-day':
+                                return diffInHours >= 24;
+                            case 'hour':
+                                return diffInHours <= 1;
+                            case 'day':
+                                return diffInHours <= 24 && diffInHours > 1;
+                            case 'week':
+                                return diffInHours <= 168 && diffInHours > 24;
+                            default:
+                                return true;
+                        }
+                    }}
+                    sorter={(a: TaskDto, b: TaskDto) => {
+                        const scheduleA = getSchedule(stringToArray(a.cron));
+                        const scheduleB = getSchedule(stringToArray(b.cron));
+                        const nextRunA = scheduleA.next();
+                        const nextRunB = scheduleB.next();
+                        return nextRunA - nextRunB;
+                    }}
                 />
                 <Table.Column
                     dataIndex="isEnable"
@@ -66,6 +138,12 @@ export default function TaskList() {
                             unCheckedChildren={false}
                         />
                     )}
+                    filters={[
+                        { text: 'On', value: true },
+                        { text: 'Off', value: false },
+                    ]}
+                    onFilter={(value, record) => record.isEnable === value}
+                    sorter={(a: TaskDto, b: TaskDto) => Number(a.isEnable) - Number(b.isEnable)}
                 />
                 <Table.Column
                     title={'Actions'}
