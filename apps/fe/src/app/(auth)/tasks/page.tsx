f1155593779c960a662acbfeedbce881311f1625
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-08 13:16:53 +0700

    feat(fe): :sparkles: add switch to quick on/off task

--- apps/fe/src/app/(auth)/tasks/page.tsx
+++ apps/fe/src/app/(auth)/tasks/page.tsx
@@ -2,21 +2,21 @@
 
 import { useUpdate } from '@refinedev/core';
 import { DeleteButton, EditButton, List, ShowButton, useTable } from '@refinedev/antd';
-import { Space, Table, Switch } from 'antd';
+import { Space, Table, Switch, Typography } from 'antd';
+import { toString as cronReadable } from 'cronstrue';
 
 import { type TaskDto } from '~be/app/tasks/dtos';
 import { useDebouncedCallback } from '~/hooks/useDebouncedCallback';
+import { HttpMethodTag } from '~/components/tag/http-method-tag';
+
+const { Text } = Typography;
 
 export default function TaskList() {
     const { tableProps } = useTable<TaskDto>({
         syncWithLocation: true,
     });
 
-    const { mutate: update } = useUpdate<TaskDto>({
-        mutationOptions: {
-            cacheTime: 1000,
-        },
-    });
+    const { mutate: update } = useUpdate<TaskDto>({});
 
     const debouncedUpdate = useDebouncedCallback(update, 200);
 
@@ -24,8 +24,29 @@ export default function TaskList() {
         <List>
             <Table<TaskDto> {...tableProps} rowKey="_id">
                 <Table.Column dataIndex="name" title={'Name'} />
-                <Table.Column dataIndex="method" title={'Method'} />
-                <Table.Column dataIndex="cron" title={'Cron Expression'} />
+                <Table.Column
+                    dataIndex="method"
+                    title={'Method'}
+                    render={(method) => <HttpMethodTag method={method} />}
+                />
+                <Table.Column
+                    dataIndex="cron"
+                    title={'Cron'}
+                    render={(_, record: TaskDto) => (
+                        <>
+                            <Space direction="vertical">
+                                <Text>{record.cron}</Text>
+                                <Text type="secondary">
+                                    {cronReadable(record.cron, {
+                                        throwExceptionOnParseError: false,
+                                        locale: 'en',
+                                        use24HourTimeFormat: true,
+                                    })}
+                                </Text>
+                            </Space>
+                        </>
+                    )}
+                />
                 <Table.Column
                     dataIndex="isEnable"
                     title={'On/Off'}
