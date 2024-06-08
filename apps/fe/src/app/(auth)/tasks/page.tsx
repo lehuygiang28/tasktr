ce7c5d1aed4fcf0ebc8a1e2b44b3aa09bdfdfc42
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-08 12:08:48 +0700

    feat(fe): add switch to enable/disable tasks in task list

--- apps/fe/src/app/(auth)/tasks/page.tsx
+++ apps/fe/src/app/(auth)/tasks/page.tsx
@@ -1,21 +1,51 @@
 'use client';
 
+import { useUpdate } from '@refinedev/core';
 import { DeleteButton, EditButton, List, ShowButton, useTable } from '@refinedev/antd';
-import { Space, Table } from 'antd';
+import { Space, Table, Switch } from 'antd';
 
 import { type TaskDto } from '~be/app/tasks/dtos';
+import { useDebouncedCallback } from '~/hooks/useDebouncedCallback';
 
 export default function TaskList() {
     const { tableProps } = useTable<TaskDto>({
         syncWithLocation: true,
     });
 
+    const { mutate: update } = useUpdate<TaskDto>({
+        mutationOptions: {
+            cacheTime: 1000,
+        },
+    });
+
+    const debouncedUpdate = useDebouncedCallback(update, 200);
+
     return (
         <List>
             <Table<TaskDto> {...tableProps} rowKey="_id">
-                <Table.Column dataIndex="name" title={'name'} />
-                <Table.Column dataIndex="method" title={'method'} />
-                <Table.Column dataIndex="cron" title={'cron'} />
+                <Table.Column dataIndex="name" title={'Name'} />
+                <Table.Column dataIndex="method" title={'Method'} />
+                <Table.Column dataIndex="cron" title={'Cron Expression'} />
+                <Table.Column
+                    dataIndex="isEnable"
+                    title={'On/Off'}
+                    render={(_, record: TaskDto) => (
+                        <Switch
+                            onChange={(val) =>
+                                debouncedUpdate({
+                                    resource: 'tasks',
+                                    id: record._id.toString(),
+                                    values: { isEnable: val },
+                                    mutationMode: 'optimistic',
+                                    successNotification: false,
+                                })
+                            }
+                            checked={record.isEnable}
+                            checkedChildren={true}
+                            unCheckedChildren={false}
+                        />
+                    )}
+                />
                 <Table.Column
                     title={'Actions'}
                     dataIndex="actions"
