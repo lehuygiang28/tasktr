5d86d739df2d6a8e5c4781def73e357445c00c58
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-11 12:54:07 +0700

    chore(be): :rocket: config serverless

--- api/index.ts
+++ api/index.ts
@@ -6,13 +6,11 @@ const tsConfigPaths = require('tsconfig-paths');
 const tsConfig = require('./tsconfig.json');
 
 const baseUrl = '../';
-const cleanup = tsConfigPaths.register({
+
+tsConfigPaths.register({
     baseUrl: tsConfig.compilerOptions.baseUrl || baseUrl,
     paths: tsConfig.compilerOptions.paths,
     cwd: './tsconfig.json',
 });
 
 require('../apps/be/src/main');
-
-// When path registration is no longer needed
-cleanup();
