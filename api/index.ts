ca2c45a9c0cd299af9aa0959316247f3981d3955
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-11 13:07:44 +0700

    chore(be): :rocket: config serverless

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,8 @@
  * Config for using path-aliases
  */
 
+console.log(process.cwd());
+
 const tsConfigPaths = require('tsconfig-paths');
 const tsConfig = require('./tsconfig.json');
 
@@ -10,6 +12,7 @@ const baseUrl = '../';
 tsConfigPaths.register({
     baseUrl: tsConfig.compilerOptions.baseUrl || baseUrl,
     paths: tsConfig.compilerOptions.paths,
+    cwd: __dirname,
 });
 
 require('../apps/be/src/main');
