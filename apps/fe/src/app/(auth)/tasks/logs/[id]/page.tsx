652f87a3c80bd439720e3e5067c6ffbbcf706b83
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-16 15:57:12 +0700

    feat(fe): :sparkles: update ui for log details

--- apps/fe/src/app/(auth)/tasks/logs/[id]/page.tsx
+++ apps/fe/src/app/(auth)/tasks/logs/[id]/page.tsx
@@ -3,23 +3,41 @@
 import Link from 'next/link';
 import { HttpError, useParsed } from '@refinedev/core';
 import { List, ShowButton, useTable } from '@refinedev/antd';
-import { Breadcrumb, Space, Table, Tag } from 'antd';
-import {
-    LineChart,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis,
-    YAxis,
-    Line,
-    Legend,
-    Label,
-} from 'recharts';
+import { Breadcrumb, Space, Table, Tag, Modal, Typography, Descriptions } from 'antd';
 import { format } from 'date-fns';
+import { useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ChartData,
+    PointElement,
+    LineElement,
+} from 'chart.js';
+import { Line, Bar } from 'react-chartjs-2';
 
 import { HttpMethodTag } from '~/components/tag/http-method-tag';
 import { HttpMethodEnum } from '~be/app/tasks/tasks.enum';
 import { type TaskLogDto } from '~be/app/task-logs';
 import { formatDateToHumanReadable, sortArrayByKey } from '~/libs/utils/common';
+
+const { Title: TextTitle, Text } = Typography;
+const { Item: DesItem } = Descriptions;
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    LineElement,
+    BarElement,
+    PointElement,
+    Title,
+    Tooltip,
+    Legend,
+);
 
 export default function LogList() {
     const { pathname } = useParsed();
@@ -33,6 +51,96 @@ export default function LogList() {
             mode: 'server',
         },
     });
+
+    const [selectedLog, setSelectedLog] = useState<TaskLogDto | null>(null);
+
+    const sortedData = sortArrayByKey(data?.data, 'executedAt');
+    const chartData: ChartData<'line'> = {
+        labels: sortedData?.map((log) => format(new Date(log.executedAt), 'HH:mm:ss dd/MM/yy')),
+        datasets: [
+            {
+                label: 'Duration',
+                data: sortedData?.map((log) => log.timings?.total),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'Wait',
+                data: sortedData?.map((log) => log.timings?.wait),
+                borderColor: 'rgb(255, 205, 86)',
+                backgroundColor: 'rgba(255, 205, 86, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'DNS',
+                data: sortedData?.map((log) => log.timings?.dns),
+                borderColor: 'rgb(75, 192, 192)',
+                backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'TCP',
+                data: sortedData?.map((log) => log.timings?.tcp),
+                borderColor: 'rgb(153, 102, 255)',
+                backgroundColor: 'rgba(153, 102, 255, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'TLS',
+                data: sortedData?.map((log) => log.timings?.tls),
+                borderColor: 'rgb(255, 159, 64)',
+                backgroundColor: 'rgba(255, 159, 64, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'Request',
+                data: sortedData?.map((log) => log.timings?.request),
+                borderColor: 'rgb(54, 162, 235)',
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'First Byte',
+                data: sortedData?.map((log) => log.timings?.firstByte),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'Download',
+                data: sortedData?.map((log) => log.timings?.download),
+                borderColor: 'rgb(201, 203, 207)',
+                backgroundColor: 'rgba(201, 203, 207, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y',
+            },
+            {
+                label: 'Response Size (KB)',
+                data: sortedData?.map((log) => Number(log.responseSizeBytes / 1024)),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                tension: 0.4,
+                pointRadius: 0,
+                yAxisID: 'y1',
+            },
+        ],
+    };
 
     return (
         <>
@@ -50,43 +158,53 @@ export default function LogList() {
             </Breadcrumb>
             <List>
                 <Space direction="vertical" style={{ width: '100%' }}>
-                    <ResponsiveContainer width="100%" height={300}>
-                        <LineChart
-                            margin={{ top: 5, right: 30, left: 20, bottom: 50 }}
-                            data={sortArrayByKey(data?.data, 'executedAt')?.map((log) => ({
-                                ...log,
-                                executedAt: format(new Date(log.executedAt), 'H:m:s d/M/yy'),
-                            }))}
-                        >
-                            <XAxis
-                                dataKey={'executedAt'}
-                                name="Executed At"
-                                angle={-30}
-                                textAnchor="end"
-                                label={
-                                    <Label
-                                        value="Executed At"
-                                        position="insideBottom"
-                                        offset={-50}
-                                        style={{ textAnchor: 'middle' }}
-                                    />
-                                }
-                            />
-                            <YAxis
-                                dataKey={'duration'}
-                                name="Duration"
-                                label={{
-                                    value: 'Duration',
-                                    position: 'insideLeft',
-                                    angle: -90,
-                                }}
-                            />
-                            <Legend verticalAlign="top" height={15} />
-                            <Line type="monotone" dataKey="duration" stroke="#82ca9d" />
-                            <Line type="monotone" dataKey="responseSizeBytes" stroke="#8884d8" />
-                            <Tooltip />
-                        </LineChart>
-                    </ResponsiveContainer>
+                    <Line
+                        style={{ width: '100%', maxHeight: '500px' }}
+                        data={chartData}
+                        options={{
+                            responsive: true,
+                            plugins: {
+                                title: {
+                                    text: 'Task execution timings and size response',
+                                    display: true,
+                                },
+                                legend: {
+                                    position: 'top' as const,
+                                },
+                                tooltip: {
+                                    mode: 'index',
+                                },
+                            },
+                            scales: {
+                                y: {
+                                    type: 'linear',
+                                    display: true,
+                                    position: 'left' as const,
+                                    title: {
+                                        display: true,
+                                        text: 'Timings (ms)',
+                                    },
+                                },
+                                y1: {
+                                    type: 'linear',
+                                    display: true,
+                                    position: 'right' as const,
+                                    title: {
+                                        display: true,
+                                        text: 'Response Size (KB)',
+                                    },
+                                },
+                            },
+                            interaction: {
+                                mode: 'index',
+                                intersect: false,
+                            },
+                            animation: {
+                                duration: 1000,
+                                easing: 'easeOutQuart',
+                            },
+                        }}
+                    />
 
                     <Table<TaskLogDto>
                         {...tableProps}
@@ -185,7 +303,11 @@ export default function LogList() {
                             dataIndex="actions"
                             render={(_, record: TaskLogDto) => (
                                 <Space>
-                                    <ShowButton size="small" recordItemId={record._id.toString()}>
+                                    <ShowButton
+                                        size="small"
+                                        recordItemId={record._id.toString()}
+                                        onClick={() => setSelectedLog(record)}
+                                    >
                                         Details
                                     </ShowButton>
                                 </Space>
@@ -194,6 +316,152 @@ export default function LogList() {
                     </Table>
                 </Space>
             </List>
+            <Modal
+                open={!!selectedLog}
+                onCancel={() => setSelectedLog(null)}
+                footer={null}
+                width={'70%'}
+            >
+                {selectedLog && (
+                    <div>
+                        <Space direction="vertical">
+                            <Descriptions
+                                layout="vertical"
+                                title={<TextTitle level={3}>Task Log Details</TextTitle>}
+                            >
+                                <DesItem label="ID">
+                                    <Text>{selectedLog._id.toString()}</Text>
+                                </DesItem>
+                                <DesItem label="Task ID">
+                                    <Text>{selectedLog.taskId.toString()}</Text>
+                                </DesItem>
+                                <DesItem label="Endpoint">
+                                    <Text>{selectedLog.endpoint}</Text>
+                                </DesItem>
+                                <DesItem label="Method">{selectedLog.method}</DesItem>
+                                <DesItem label="Status Code">{selectedLog.statusCode}</DesItem>
+                                <DesItem label="Worker Name">
+                                    <Text>{selectedLog.workerName}</Text>
+                                </DesItem>
+                            </Descriptions>
+
+                            <Descriptions
+                                layout="vertical"
+                                title={<TextTitle level={3}>Timings</TextTitle>}
+                            >
+                                <DesItem label="Executed At">
+                                    <Text>{formatDateToHumanReadable(selectedLog.executedAt)}</Text>
+                                </DesItem>
+                                <DesItem label="Scheduled At">
+                                    <Text>
+                                        {formatDateToHumanReadable(selectedLog.scheduledAt)}
+                                    </Text>
+                                </DesItem>
+                                <DesItem label="Duration">
+                                    <Text>{selectedLog.timings?.total} ms</Text>
+                                </DesItem>
+                                <DesItem label="Response Size">
+                                    <Text>
+                                        {(selectedLog.responseSizeBytes / 1024).toFixed(2)} KB
+                                    </Text>
+                                </DesItem>
+                            </Descriptions>
+
+                            {selectedLog.timings && (
+                                <div
+                                    style={{
+                                        maxHeight: '400px',
+                                        maxWidth: '100%',
+                                        marginBottom: 20,
+                                    }}
+                                >
+                                    <Typography.Title level={5}>Detailed Timings</Typography.Title>
+                                    <div style={{ height: '70%', width: '100%' }}>
+                                        <Bar
+                                            data={{
+                                                labels: ['Duration (ms)'],
+                                                datasets: [
+                                                    {
+                                                        label: 'Wait Time',
+                                                        data: [selectedLog.timings.wait],
+                                                        backgroundColor: 'rgb(255, 99, 132)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'DNS Lookup',
+                                                        data: [selectedLog.timings.dns],
+                                                        backgroundColor: 'rgb(255, 159, 64)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'TCP Connection',
+                                                        data: [selectedLog.timings.tcp],
+                                                        backgroundColor: 'rgb(255, 205, 86)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'TLS Handshake',
+                                                        data: [selectedLog.timings.tls],
+                                                        backgroundColor: 'rgb(75, 192, 192)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'Request Time',
+                                                        data: [selectedLog.timings.request],
+                                                        backgroundColor: 'rgb(54, 162, 235)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'Time to First Byte',
+                                                        data: [selectedLog.timings.firstByte],
+                                                        backgroundColor: 'rgb(153, 102, 255)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'Download Time',
+                                                        data: [selectedLog.timings.download],
+                                                        backgroundColor: 'rgb(201, 203, 207)',
+                                                        stack: 'timings',
+                                                    },
+                                                    {
+                                                        label: 'Total',
+                                                        data: [selectedLog.timings.total],
+                                                        backgroundColor: 'rgb(0, 128, 0)',
+                                                    },
+                                                ],
+                                            }}
+                                            options={{
+                                                indexAxis: 'y',
+                                                responsive: true,
+                                                scales: {
+                                                    x: {
+                                                        stacked: true,
+                                                    },
+                                                },
+                                                plugins: {
+                                                    legend: {
+                                                        position: 'bottom',
+                                                        display: true,
+                                                    },
+                                                    tooltip: {
+                                                        mode: 'index',
+                                                        intersect: false,
+                                                    },
+                                                },
+                                                maintainAspectRatio: false,
+                                                hover: {
+                                                    mode: 'index',
+                                                    intersect: false,
+                                                },
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            )}
+                        </Space>
+                    </div>
+                )}
+            </Modal>
         </>
     );
 }
