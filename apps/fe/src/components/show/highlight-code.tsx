4d92128a14aea770bb32d51afb00955a9dccea0f
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-22 02:19:03 +0700

    feat(fe): add auto language detection

--- apps/fe/src/components/show/highlight-code.tsx
+++ apps/fe/src/components/show/highlight-code.tsx
@@ -13,57 +13,85 @@ import { ColorModeContext } from '~/contexts/color-mode';
 
 export type HighlightCodeProps = {
     source: string;
-    formatType: 'json' | 'markdown' | 'html';
+    formatType: 'json' | 'markdown' | 'html' | 'xml' | 'auto';
 };
 
 export function HighlightCode({ source = '', formatType }: HighlightCodeProps) {
     const { mode } = useContext(ColorModeContext);
     const [formattedSource, setFormattedSource] = useState<string>('');
     const [isFormatting, setIsFormatting] = useState(false);
+    const [language, setLanguage] = useState<HighlightCodeProps['formatType']>(formatType);
 
     useEffect(() => {
         const formatSource = async () => {
             setIsFormatting(true);
-            switch (formatType) {
-                case 'json': {
-                    const formatted = await format(source, {
-                        plugins: [prettierEstree, prettierBabel],
-                        parser: 'json-stringify',
-                    });
-                    setFormattedSource(formatted?.trim());
-                    break;
+            const trimmedSource = source?.trim();
+            try {
+                switch (language) {
+                    case 'auto': {
+                        if (/<!DOCTYPE html>/i.test(source)) {
+                            setLanguage('html');
+                        } else if (/^<\?xml/i.test(source)) {
+                            setLanguage('xml');
+                        } else if (/^\s*\{/.test(source)) {
+                            setLanguage('json');
+                        } else {
+                            // Fallback to markdown
+                            throw new Error('Non-auto language detected. Fallback to markdown.');
+                        }
+                        break;
+                    }
+                    case 'json': {
+                        const formatted = await format(trimmedSource, {
+                            plugins: [prettierEstree, prettierBabel],
+                            parser: 'json-stringify',
+                        });
+                        setFormattedSource(formatted?.trim());
+                        break;
+                    }
+                    case 'html': {
+                        const formatted = await format(trimmedSource, {
+                            plugins: [prettierEstree, prettierHtml],
+                            parser: 'html',
+                        });
+                        setFormattedSource(formatted?.trim());
+                        break;
+                    }
+                    case 'xml': {
+                        const formatted = await format(trimmedSource, {
+                            plugins: [prettierEstree, prettierHtml],
+                            parser: 'xml',
+                        });
+                        setFormattedSource(formatted?.trim());
+                        break;
+                    }
+                    case 'markdown':
+                    default: {
+                        const formatted = await format(trimmedSource, {
+                            plugins: [prettierEstree, prettierMd],
+                            parser: 'markdown',
+                        });
+                        setFormattedSource(formatted?.trim());
+                        break;
+                    }
                 }
-                case 'html': {
-                    const formatted = await format(source, {
-                        plugins: [prettierEstree, prettierHtml],
-                        parser: 'html',
-                    });
-                    setFormattedSource(formatted?.trim());
-                    break;
-                }
-                case 'markdown':
-                default: {
-                    const formatted = await format(source, {
-                        plugins: [prettierEstree, prettierMd],
-                        parser: 'markdown',
-                    });
-                    setFormattedSource(formatted?.trim());
-                    break;
-                }
+            } catch {
+                setLanguage('markdown');
             }
+
             setIsFormatting(false);
         };
 
         formatSource();
-    }, [source, formatType]);
+    }, [source, language]);
 
     return isFormatting ? (
         <Skeleton style={{ width: '100%' }} active />
     ) : (
         <Highlight
             theme={mode === 'light' ? themes.duotoneLight : themes.vsDark}
-            code={formattedSource?.trim() === '' ? '// empty' : formattedSource}
-            language="ts"
+            code={formattedSource}
+            language={language}
         >
             {({ style, tokens, getLineProps, getTokenProps }) => (
                 <pre style={{ ...style, whiteSpace: 'pre-wrap' }}>
