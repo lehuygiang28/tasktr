3eb8e0cc7868f968fd68ab01c1d0fa826b304385
lehuygiang28 <lehuygiang28@gmail.com>
2024-06-24 12:16:16 +0700

    chore: :zap: add date to pong message

--- apps/be/src/app/app.service.ts
+++ apps/be/src/app/app.service.ts
@@ -1,11 +1,13 @@
 import { Response } from 'express';
-
 import { Injectable } from '@nestjs/common';
+import { format } from 'date-fns/format';
 
 @Injectable()
 export class AppService {
     getPing(res: Response) {
-        res.header('Cache-Control', 'no-store, max-age=0, must-revalidate').send('pong');
+        res.header('Cache-Control', 'no-store, max-age=0, must-revalidate')
+            .header('Content-Type', 'text/plain')
+            .send(`pong ${format(new Date(), 'HH:mm:ss:SSS dd/MM/yyyy')}`);
         return;
     }
 }
--- apps/fe/src/app/api/ping/route.ts
+++ apps/fe/src/app/api/ping/route.ts
@@ -1,7 +1,11 @@
-export async function GET(request: Request) {
-    return new Response('pong', {
+import { NextRequest, NextResponse } from 'next/server';
+import { format } from 'date-fns/format';
+
+export async function GET(request: NextRequest) {
+    return new NextResponse(`pong ${format(new Date(), 'HH:mm:ss:SSS dd/MM/yyyy')}`, {
         headers: {
             'Cache-Control': 'no-store, max-age=0, must-revalidate',
+            'Content-Type': 'text/plain',
         },
     });
 }
