05a80f747540f91a957a06b0f4c65aacc2d526e0
lehuygiang28 <lehuygiang28@gmail.com>
2024-08-24 00:48:04 +0700

    refactor(fe): :art: move block/unblock button to component

--- apps/fe/src/app/(admin)/users/show/[id]/page.tsx
+++ apps/fe/src/app/(admin)/users/show/[id]/page.tsx
@@ -1,86 +1,22 @@
 'use client';
 
-import {
-    Descriptions,
-    Tag,
-    Typography,
-    Image,
-    Row,
-    Col,
-    Button,
-    Modal,
-    Select,
-    Input,
-    Divider,
-} from 'antd';
+import { Descriptions, Tag, Typography, Image, Row, Col, Divider } from 'antd';
 import { Show } from '@refinedev/antd';
-import { useShow, useUpdate, useGetIdentity, useParsed } from '@refinedev/core';
+import { useParsed, useShow } from '@refinedev/core';
 
-import type { UserDto, BlockUserDto } from '~be/app/users/dtos';
+import type { UserDto } from '~be/app/users/dtos';
 import { formatDateToHumanReadable } from '~/libs/utils/common';
-import { useState } from 'react';
 import { BlockLogTable } from '~/components/table/block-log';
+import { BlockOrUnblockUser } from '~/components/button/block-or-unblock-user';
 
 const { Text } = Typography;
-const { Option } = Select;
-const { TextArea } = Input;
-
-const DEFAULT_BLOCK_REASON = 'Spam';
 
 export default function UserShow() {
-    const { data: me, isLoading: isIdentityLoading } = useGetIdentity<UserDto>();
     const { id } = useParsed();
 
     const {
         queryResult: { data: { data: record } = {}, isLoading },
     } = useShow<UserDto>({});
-
-    const { mutate } = useUpdate({
-        mutationOptions: {},
-    });
-
-    const [isBlockModalVisible, setIsBlockModalVisible] = useState(false);
-    const [isUnblockModalVisible, setIsUnblockModalVisible] = useState(false);
-    const [blockReason, setBlockReason] = useState(DEFAULT_BLOCK_REASON);
-    const [customReason, setCustomReason] = useState('');
-
-    const handleBlock = (data: BlockUserDto) => {
-        mutate({
-            resource: 'users/block',
-            id: record?._id.toString(),
-            values: { ...data, reason: blockReason === 'Other' ? customReason : blockReason },
-            invalidates: ['all'],
-        });
-        setIsBlockModalVisible(false);
-    };
-
-    const handleUnblock = () => {
-        mutate({
-            resource: 'users/unblock',
-            id: record?._id.toString(),
-            values: {},
-            invalidates: ['all'],
-        });
-        setIsUnblockModalVisible(false);
-    };
-
-    const handleBlockReasonChange = (value: string) => {
-        setBlockReason(value);
-        if (value !== 'Other') {
-            setCustomReason('');
-        }
-    };
-
-    const handleCustomReasonChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setCustomReason(e.target.value);
-    };
-
-    const restoreDefault = () => {
-        setBlockReason(DEFAULT_BLOCK_REASON);
-        setCustomReason('');
-        setIsBlockModalVisible(false);
-        setIsUnblockModalVisible(false);
-    };
 
     return (
         <Show
@@ -89,69 +25,10 @@ export default function UserShow() {
             headerButtons={({ defaultButtons }) => (
                 <>
                     {defaultButtons}
-                    {!isIdentityLoading &&
-                        me?._id?.toString() !== id?.toString() &&
-                        (record?.block?.isBlocked ? (
-                            <Button onClick={() => setIsUnblockModalVisible(true)}>Unblock</Button>
-                        ) : (
-                            <Button danger onClick={() => setIsBlockModalVisible(true)}>
-                                Block
-                            </Button>
-                        ))}
+                    <BlockOrUnblockUser idParam={id} user={record} />
                 </>
             )}
         >
-            <>
-                <Modal
-                    title="Confirm Block"
-                    open={isBlockModalVisible}
-                    onOk={() => {
-                        handleBlock({
-                            reason: blockReason === 'Other' ? customReason : blockReason,
-                        });
-                        restoreDefault();
-                    }}
-                    onCancel={() => {
-                        setIsBlockModalVisible(false);
-                        restoreDefault();
-                    }}
-                    destroyOnClose
-                >
-                    <p>Are you sure you want to block this user?</p>
-                    <Select
-                        title="Please select a reason"
-                        defaultValue="Select a reason"
-                        value={blockReason}
-                        onChange={handleBlockReasonChange}
-                        style={{ width: '100%' }}
-                    >
-                        <Option value={DEFAULT_BLOCK_REASON}>{DEFAULT_BLOCK_REASON}</Option>
-                        <Option value="Other">Other</Option>
-                    </Select>
-                    {blockReason === 'Other' && (
-                        <TextArea
-                            placeholder="Please specify the reason"
-                            value={customReason}
-                            onChange={handleCustomReasonChange}
-                            style={{ marginTop: 16 }}
-                        />
-                    )}
-                </Modal>
-                <Modal
-                    title="Confirm Unblock"
-                    open={isUnblockModalVisible}
-                    onOk={() => {
-                        handleUnblock();
-                        restoreDefault();
-                    }}
-                    onCancel={() => {
-                        setIsUnblockModalVisible(false);
-                        restoreDefault();
-                    }}
-                >
-                    <p>Are you sure you want to unblock this user?</p>
-                </Modal>
-            </>
             <div>
                 <Row style={{ width: '100%' }}>
                     <Col xs={24} md={4}>
