bcbb8295306e897ac9bcac42a12bfb902af55918
lehuygiang28 <lehuygiang28@gmail.com>
2024-08-26 11:55:20 +0700

    feat(fe): :zap: handling if user have no tasks to export

--- apps/fe/src/components/button/export-task.tsx
+++ apps/fe/src/components/button/export-task.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { useExport } from '@refinedev/core';
+import { useExport, useNotification, useList } from '@refinedev/core';
 import { ExportButton } from '@refinedev/antd';
+import { Spin } from 'antd';
 
 import type { TaskDto, TaskImport } from '~be/app/tasks/dtos';
 
 export function ExportTask() {
+    const { open } = useNotification();
+    const { data: { data: tasks = [] } = {} } = useList({
+        resource: 'tasks',
+        pagination: {
+            pageSize: 1,
+            mode: 'server',
+        },
+    });
     const { triggerExport, isLoading: exportLoading } = useExport<TaskDto>({
+        resource: 'tasks',
+        pageSize: 50,
         mapData: (item) => {
             const { cronHistory, _id, userId, createdAt, updatedAt, options, ...rest } = item;
             return {
@@ -13,10 +24,32 @@ export function ExportTask() {
                 options: options ? JSON.stringify(options) : '',
             } satisfies Omit<TaskImport, 'options'> & { options?: string };
         },
+        onError: (error) => {
+            console.log(error);
+            open({
+                type: 'error',
+                message: 'Error',
+                description: 'Export failed',
+            });
+        },
     });
+
+    const handleOnExport = () => {
+        if (tasks?.length <= 0) {
+            open({
+                type: 'error',
+                message: 'Error',
+                description: 'You have no tasks to export',
+            });
+            return;
+        }
+
+        triggerExport();
+    };
     return (
         <>
-            <ExportButton onClick={triggerExport} loading={exportLoading} />
+            <ExportButton onClick={handleOnExport} loading={exportLoading} />
+            {exportLoading && <Spin size="large" fullscreen />}
         </>
     );
 }
